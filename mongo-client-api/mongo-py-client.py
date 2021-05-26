05ac6efce4e7b5b4cf962e38aaafdb38a0513c5d
Ricardo Garmendia <rdgc91@gmail.com>
2021-05-26 19:44:04 -0400

    :children_crossing:  Work in progress

--- mongo-client-api/mongo-py-client.py
+++ mongo-client-api/mongo-py-client.py
@@ -3,15 +3,16 @@ from flask import Flask
 from flask import jsonify
 from jaeger_client.constants import TRACE_ID_HEADER
 import requests
-import jaeger_config
 import json
 from opentracing.propagation import Format
+import logging
+from jaeger_client import Config
 
 app = Flask(__name__)
 
 @app.route('/seekstar', methods=['GET'])
 def seek_star():
-    tracer = jaeger_config.init_tracer('seeker')
+    tracer = init_tracer('seeker')
     with tracer.start_span('star-seeker') as span:
         span.log_kv({'event': 'star-seeker-log', 'life': 42})
         span.set_tag('seekstars', "star")
@@ -24,6 +25,24 @@ def seek_star():
 
         output = requests.get("http://127.0.0.1:5000/star", headers=headers) #,  headers=headers_dict)
         return {'result': output.json()}
+
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
 
 if __name__ == '__main__':
     app.run(debug=True, port=6000)
