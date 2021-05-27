197f323cd0fb8ebd88588baff58e9328593b4901
Manuel Roldan <mroldand27@gmail.com>
2021-05-26 20:14:35 -0400

    :rocket: it worked!

--- mongo-client-api/mongo-py-client.py
+++ mongo-client-api/mongo-py-client.py
@@ -1,18 +1,47 @@
 
+from os import name
 from flask import Flask
 from flask import jsonify
 from jaeger_client.constants import TRACE_ID_HEADER
+import opentracing
 import requests
 import json
 from opentracing.propagation import Format
+from opentracing import tracer
 import logging
 from jaeger_client import Config
+from requests.api import head
 
 app = Flask(__name__)
+class Tracer:
+    client = None
+    service_name = ''
+    
+    def __init__(self, service_name):
+        self.client = None
+        self.service_name = service_name
+
+    def init_tracer(self):
+        logging.getLogger('').handlers = []
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+        config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=self.service_name,
+        )
+
+        # this call also sets opentracing.tracer
+        return config.initialize_tracer()
 
 @app.route('/seekstar', methods=['GET'])
 def seek_star():
-    tracer = init_tracer('seeker')
+    tracer = opentracing.tracer
     with tracer.start_span('star-seeker') as span:
         span.log_kv({'event': 'star-seeker-log', 'life': 42})
         span.set_tag('seekstars', "star")
@@ -22,27 +51,11 @@ def seek_star():
             span_context = span.context,
             format = Format.HTTP_HEADERS,
             carrier=headers)
-
         output = requests.get("http://127.0.0.1:5000/star", headers=headers) #,  headers=headers_dict)
-        return {'result': output.json()}
 
-def init_tracer(service):
-    logging.getLogger('').handlers = []
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-
-    config = Config(
-        config={
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            'logging': True,
-        },
-        service_name=service,
-    )
-
-    # this call also sets opentracing.tracer
-    return config.initialize_tracer()
+    return {'result': output.json()}
 
 if __name__ == '__main__':
+    tracer = Tracer(service_name='seeker')
+    tracer.init_tracer()
     app.run(debug=True, port=6000)
--- mongo-web-api/mongo.py
+++ mongo-web-api/mongo.py
@@ -1,29 +1,56 @@
-from flask import Flask
+from flask import Flask, json
 from flask import jsonify
 from flask import request
+import opentracing
 from flask_pymongo import PyMongo
 import logging
 from jaeger_client import Config
 from opentracing.propagation import Format
+from opentracing import tracer
 
 app = Flask(__name__)
 
 app.config['MONGO_URI'] = 'mongodb://ricardo:password@localhost:27017/josalys'
 
 mongo = PyMongo(app)
+class Tracer:
+    service_name = ''
+    
+    def __init__(self, service_name):
+        self.service_name = service_name
 
-tracer = None
+    def init_tracer(self):
+        logging.getLogger('').handlers = []
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+        config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=self.service_name,
+        )
+
+        # this call also sets opentracing.tracer
+        return config.initialize_tracer()
 
 @app.route('/star', methods=['GET'])
 def get_all_stars():
+    tracer = opentracing.tracer
     father_span = tracer.extract(Format.HTTP_HEADERS, request.headers)
     with tracer.start_span('star-seeker', child_of=father_span) as span:
       star = mongo.db.stars
-      span.log({"star": star})
+      span.log_kv({"operation": star})
       output = []
       for s in star.find():
-          output.append({'name': s['name'], 'distance': s['distance']})
-      return jsonify({'result': output})
+          output.append({'name': s['name']})
+      jsonOutput = jsonify({'result': output})
+      span.log_kv({"result": jsonOutput})
+    span.finish()
+    return jsonOutput
 
 @app.route('/star/<name>', methods=['GET'])
 def get_one_star(name):
@@ -34,24 +61,6 @@ def get_one_star(name):
   else:
     output = "No such name"
   return jsonify({'result' : output})
-
-def init_tracer(service):
-    logging.getLogger('').handlers = []
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-
-    config = Config(
-        config={
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            'logging': True,
-        }, 
-        service_name=service,
-    )
-
-    # this call also sets opentracing.tracer
-    tracer = config.initialize_tracer()
 
 @app.route('/star', methods=['POST'])
 def add_star():
@@ -64,4 +73,6 @@ def add_star():
   return jsonify({'result' : output})
 
 if __name__ == '__main__':
+    tracer = Tracer(service_name='star-seeker')
+    tracer.init_tracer()
     app.run(debug=True)
